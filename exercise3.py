6a63ef980bde2a926f8493433018d67ca95099f2
CarlHaufe <113439167+CarlHaufe@users.noreply.github.com>
2024-10-07 17:46:08 +0200

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -30,7 +30,7 @@ class Rectangle:
         # Task A: remove duplication by defining a function
         #         that checks if a value is within an interval
         #         and reuse that here.
-        return self.is_in_interval(point)
+        return self.is_in_interval(point, tolerance)
         # ll_px = point.x - self._lower_left.x
         # ll_py = point.y - self._lower_left.y
         # return ll_px >= 0 and ll_px <= self._dx \
@@ -42,7 +42,7 @@ class Rectangle:
     def _is_idx_on_right_edge(self, i: int) -> bool:
         return i in [1, 3]
 
-    def is_in_interval(self, point) -> bool: # Task A
+    def is_in_interval(self, point, tolerance) -> bool: # Task A
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
         if ll_px >= -tolerance and ll_px <= self._dx + tolerance and ll_py >= -tolerance and ll_py <= self._dy + tolerance:
